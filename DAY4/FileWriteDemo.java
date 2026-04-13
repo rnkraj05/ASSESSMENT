07b9d33eb911b8db27925c05ac442bac00102023
Raunak <rnkraj05@gmail.com>
2026-04-13 12:28:56 +0530

    updated DAY4

--- DAY4/FileWriteDemo.java
+++ DAY4/FileWriteDemo.java
@@ -1,7 +1,7 @@
-// Student class (no need to make it public here)
-
+import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.io.BufferedReader;
 
 class Student {
     int id;
@@ -15,22 +15,36 @@ class Student {
 
 public class FileWriteDemo {
     public static void main(String[] args) {
-        
+
         // Create Student Object
         Student s1 = new Student(1, "Raunak");
 
-        try {
-            FileWriter fw = new FileWriter("student.txt");
+        String fileName = "student.txt";  
 
-            //Write object data
+        try {
+            // WRITE
+            FileWriter fw = new FileWriter(fileName);
+
             fw.write("ID: " + s1.id + "\n");
             fw.write("Name: " + s1.name);
 
             fw.close();
 
-            System.out.println("Data written successfully!");
+            System.out.println("Data written successfully!\n");
 
-        } catch (IOException e) {   
+            // READ
+            BufferedReader br = new BufferedReader(new FileReader(fileName));
+
+            String line;
+            System.out.println("Displaying file content:\n");
+
+            while ((line = br.readLine()) != null) {
+                System.out.println(line);
+            }
+
+            br.close();
+
+        } catch (IOException e) {
             System.out.println("Error occurred: " + e.getMessage());
         }
     }
